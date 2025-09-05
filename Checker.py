aaab973c077ba40a72399ac9383cd80f040a93f1
Vitor Oliveira <vitoroliveira@MacBook-Pro-de-Vitor.local>
2025-09-05 10:46:00 -0300

    first commit

---
+++ Checker.py
@@ -0,0 +1,48 @@
+import os
+import requests
+
+
+class Helpers:
+    @staticmethod
+    def create_folder(pathdir):
+        path_mac = '/Users/vitoroliveira/Desktop/'
+        path_kali = '/home/kali/Desktop/'
+
+        try:
+            check = os.popen("uname -a").read()
+
+            if "Darwin" in check:
+                    path = os.system(f"mkdir {path_mac}/{name}")
+                    if path == 256:
+                        print("\033[93m[*] Diretorio ja existe\033[0m")
+                    else:
+                        print(f"\033[92m[*] Diretorio criado com Sucesso! em {path_mac}\033[0m")
+                    return f'{path_mac}{name}'# Verde
+            else:
+                path = os.system(f"mkdir {path_kali}{name}")
+                if path == 256:
+                    print("\033[93m[*] Diretorio ja existe\033[0m")
+                else:
+                    print(f"\033[92m[*] Diretorio criado com Sucesso! em {path_kali}\033[0m")  # Verde
+                    return f"{path_kali}{name}"
+        except Exception as e:
+            print(e)
+
+
+    @staticmethod
+    def check_location(ip):
+        try:
+            host = requests.head(f"http://{ip}/", timeout=10).headers["Location"]
+            check = os.system(f"cat /etc/hosts | grep {ip}")
+            if check == 256:
+                clean_host = host.replace("http://", "").replace("/", "")
+                os.system(f"sudo echo '{ip}  {clean_host}' | sudo tee -a /etc/hosts")
+                print(f"\033[92m[*] {host}Adicionado com Sucesso! no /etc/hosts\033[0m")
+            if not host:
+                print(f"\033[92m[*] Não tem redirect pata um dominio\033[0m")
+            else:
+                print("\033[93m[*] Ja esta no /etc/hosts\033[0m")  # Amarelo
+            return host
+        except Exception as e:
+            print(e)
+
---
+++ labscan.py
@@ -0,0 +1,56 @@
+# TODO Fazer a validação se é linux ou windows para poder fazer o basico
+# TODO Se for Windows, rodar o basico de enumeração no AD
+# TODO Testear em host que não tem ip na maquina
+# TODO Colocar o path como env para não ter que digitar todas as vezes
+
+
+from Scan import Scan
+from argparse import ArgumentParser, RawTextHelpFormatter
+
+
+def print_banner():
+    banner = r"""
+██╗      █████╗ ██████   ███████╗ ██████╗  █████╗ ███╗  ██╗
+██║     ██╔══██╗██╔══██  ██╔════╝ ██╔════╝██╔══██╗████╗ ██║
+██║     ███████║██████   ███████  ██║     ███████║██╔██╗██║
+██║     ██╔══██║██╔══██       ██  ██║     ██╔══██║██║╚████║
+███████╗██║  ██║██████║  ███████╗ ██████╗ ██║  ██║██║ ╚███║
+╚══════╝╚═╝  ╚═╝╚═════╝  ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚══╝
+"""
+
+    print(banner)
+
+def main():
+    print_banner()
+
+    parser = ArgumentParser(
+        description="Ferramenta para automatizar o baisco de scan em CTFS.\n"
+                    "Exemplo de uso:\n"
+                    "  python labscan.py -i 192.168.0.1 --rust-scan ",
+        usage="python labscan.py -i <IP> [--rust-scan] [--nmap-scan]",
+        formatter_class=RawTextHelpFormatter
+    )
+    parser.add_argument("-i", "--ip", required=True)
+    parser.add_argument("-d", "--dir", required=True)
+    parser.add_argument("-rs", "--rust-scan", action="store_true", help="Rust scan mode")
+    parser.add_argument("-ns", "--nmap-scan", action="store_true", help="Nmap mode")
+
+    args = parser.parse_args()
+    recom = Scan(args.ip, args.dir)
+
+
+    print(f"\033[92m[*] Iniciando o recon\033[0m")
+    if args.rust_scan:
+        print(f"\033[92m[*] Iniciando Rustscan\033[0m")
+        recom.port_scan_rust()
+    if args.nmap_scan:
+        print(f"\033[92m[*] Iniciando NMAP\033[0m")
+        recom.port_scan_nmap()
+    print(f"\033[92m[*] Iniciando ffuf\033[0m")
+    recom.fuzz_directories()
+    print(f"\033[92m[*] Iniciando ffuf subdomain\033[0m")
+    recom.fuzz_subdomains()
+
+
+if __name__ == '__main__':
+    main()
